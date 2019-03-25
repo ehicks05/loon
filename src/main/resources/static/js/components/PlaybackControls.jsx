cd29136944e3186ec025e18c722d5c3f6630feba
Eric Hicks <ehicks05@yahoo.com>
2019-03-25 11:39:33 -0400

    Move playback buttons to the left side on large screens.

--- src/main/resources/static/js/components/PlaybackControls.jsx
+++ src/main/resources/static/js/components/PlaybackControls.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import Slider, { createSliderWithTooltip } from 'rc-slider';
 
 import 'rc-slider/assets/index.css';
@@ -73,91 +73,112 @@ export default class PlaybackControls extends React.Component {
         const windowWidth = this.props.store.uiState.windowDimensions.width;
         const textWidth = windowWidth > 768 ? (windowWidth - 350) + 'px' : '100%';
 
+        const trackProgressBar =
+            <div className="level-item" style={{marginBottom: '0'}}>
+                <span id="timer" style={{fontSize: '.875rem', marginRight: '8px'}}>{formattedTimeElapsed}</span>
+                <SliderWithTooltip name="progress" id="progress" style={{width: '100%', margin: '0'}}
+                                   trackStyle={{backgroundColor: 'hsl(141, 71%, 48%)', height: 4}}
+                                   railStyle={{backgroundColor: '#ddd'}}
+                                   handleStyle={{borderColor: 'hsl(141, 71%, 48%)'}}
+                                   tipFormatter={PlaybackControls.formatTime}
+                                   type="range" value={timeElapsed} max={duration} step={1} onChange={this.handleProgressChange}/>
+                <span id="duration" style={{fontSize: '.875rem', marginLeft: '8px'}}>{formattedDuration}</span>
+            </div>;
+
+        const trackDescription =
+            <div className="level-item">
+                <span id="track" style={{maxWidth: textWidth, maxHeight: '72px', overflow: 'auto'}}>
+                    <b>{title}</b>
+                    <br/>
+                    <span style={{fontSize: '.875rem'}}>
+                        <Link to={'/artist/' + artist}>{artist}</Link>
+                        &nbsp;-&nbsp;
+                        <Link to={'/artist/' + albumArtist + '/album/' + album}><i>{album}</i></Link>
+                    </span>
+                </span>
+            </div>;
+
+        const prevButton =
+            <a className="button" id="prevBtn" onClick={(e) => this.handleTrackChange(e, 'prev')}>
+                <span className="icon">
+                    <FontAwesomeIcon icon={faStepBackward}/>
+                </span>
+            </a>;
+
+        const playButton =
+            <a className="button is-medium" id="pauseBtn" onClick={(e) => this.handlePlayerStateChange(e, this.props.playerState === 'playing' ? 'paused' : 'playing')}>
+                <span className="icon">
+                    <FontAwesomeIcon icon={this.props.playerState === 'playing' ? faPause : faPlay}/>
+                </span>
+            </a>;
+
+        const nextButton =
+            <a className="button" id="nextBtn" onClick={(e) => this.handleTrackChange(e, 'next')}>
+                <span className="icon">
+                    <FontAwesomeIcon icon={faStepForward}/>
+                </span>
+            </a>;
+
+        const playbackButtons =
+            <Fragment>
+                {prevButton}
+                {playButton}
+                {nextButton}
+                {windowWidth > 768 && <span style={{paddingLeft: '8px'}}> </span>}
+            </Fragment>;
+
+        const shuffleButton =
+            <a className={"button is-small" + (shuffle ? " is-success" : "")} style={{marginLeft: '1.5em'}} id="shuffleBtn" onClick={this.handleShuffleChange}>
+                <span className="icon">
+                    <FontAwesomeIcon icon={faRandom} fixedWidth/>
+                </span>
+            </a>;
+
+        const muteButton =
+            <a className="button is-small" id="volumeBtn" style={{margin:'0 .75em 0 .5em'}} onClick={this.handleMuteChange}>
+                <span className="icon">
+                    <FontAwesomeIcon icon={muted ? faVolumeOff : faVolumeUp} fixedWidth/>
+                </span>
+            </a>;
+
+        const volumeSlider =
+            <div style={{width: '120px'}}>
+                <SliderWithTooltip trackStyle={{ backgroundColor: 'hsl(141, 71%, 48%)', height: 4 }}
+                                   railStyle={{backgroundColor: '#ddd'}}
+                                   handleStyle={{borderColor: 'hsl(141, 71%, 48%)'}}
+                                   value={volume} min={-30} max={0} step={1}
+                                   tipFormatter={v => `${v}dB`}
+                                   onChange={this.handleVolumeChange} />
+            </div>;
+
+        const levelLeft =
+            <div className="level-left">
+                {windowWidth > 768 && playbackButtons}
+                {trackDescription}
+            </div>;
+
+        const levelRight =
+            <div className="level-right" style={{marginTop: '4px', marginRight: '6px'}}>
+                <div className="level-item">
+                    {windowWidth <= 768 && playbackButtons}
+                    {shuffleButton}
+                    {muteButton}
+                    {volumeSlider}
+                </div>
+            </div>;
+
         return (
             <div>
                 <div className="section myLevel" style={{zIndex: '5', position: 'static', padding: '2px 6px 0 6px'}}>
                     <nav className="level">
-                        <div className="level-item" style={{marginBottom: '0'}}>
-                            <span id="timer" style={{fontSize: '.875rem', marginRight: '8px'}}>{formattedTimeElapsed}</span>
-                            <SliderWithTooltip name="progress" id="progress" style={{width:'100%', margin: '0'}}
-                                    trackStyle={{ backgroundColor: 'hsl(141, 71%, 48%)', height: 4 }}
-                                    railStyle={{backgroundColor: '#ddd'}}
-                                    handleStyle={{borderColor: 'hsl(141, 71%, 48%)'}}
-                                    tipFormatter={PlaybackControls.formatTime}
-                                    type="range" value={timeElapsed} max={duration} step={1} onChange={this.handleProgressChange}/>
-                            <span id="duration" style={{fontSize: '.875rem', marginLeft: '8px'}}>{formattedDuration}</span>
-                        </div>
+                        {trackProgressBar}
                     </nav>
                 </div>
+
                 <div className="section myLevel" style={{zIndex: '5', position: 'static', padding: '6px', paddingTop: '0'}}>
                     <nav className="level">
-                        <div className="level-left">
-                            <div className="level-item">
-                                <span id="track" style={{maxWidth: textWidth, maxHeight: '72px', overflow: 'auto'}}>
-                                    <b>{title}</b>
-                                    <br />
-                                    <span style={{fontSize: '.875rem'}}>
-                                        <Link to={'/artist/' + artist}>{artist}</Link>
-                                        &nbsp;-&nbsp;
-                                        <Link to={'/artist/' + albumArtist + '/album/' + album}><i>{album}</i></Link>
-                                    </span>
-                                </span>
-                            </div>
-                        </div>
-                        <div className="level-right" style={{marginTop: '4px', marginRight: '6px'}}>
-                            <div className="level-item">
-                                <a className="button" id="prevBtn" onClick={(e) => this.handleTrackChange(e, 'prev')}>
-                                    <span className="icon">
-                                        <FontAwesomeIcon icon={faStepBackward}/>
-                                    </span>
-                                </a>
-
-                                {
-                                    (this.props.playerState === 'paused' || this.props.playerState === 'stopped') ?
-                                        <a className="button is-medium" id="playBtn" onClick={(e) => this.handlePlayerStateChange(e, 'playing')}>
-                                            <span className="icon">
-                                                <FontAwesomeIcon icon={faPlay}/>
-                                            </span>
-                                        </a>
-                                        : ''
-                                }
-                                {
-                                    this.props.playerState === 'playing' ?
-                                        <a className="button is-medium" id="pauseBtn" onClick={(e) => this.handlePlayerStateChange(e, 'paused')}>
-                                            <span className="icon">
-                                                <FontAwesomeIcon icon={faPause}/>
-                                            </span>
-                                        </a>
-                                        : ''
-                                }
-
-                                <a className="button" id="nextBtn" onClick={(e) => this.handleTrackChange(e, 'next')}>
-                                    <span className="icon">
-                                        <FontAwesomeIcon icon={faStepForward}/>
-                                    </span>
-                                </a>
-
-                                <a className={"button is-small" + (shuffle ? " is-success" : "")} style={{marginLeft: '1.5em'}} id="shuffleBtn" onClick={this.handleShuffleChange}>
-                                    <span className="icon">
-                                        <FontAwesomeIcon icon={faRandom} fixedWidth/>
-                                    </span>
-                                </a>
-
-                                <a className="button is-small" id="volumeBtn" style={{margin:'0 .75em 0 .5em'}} onClick={this.handleMuteChange}>
-                                    <span className="icon">
-                                        <FontAwesomeIcon icon={muted ? faVolumeOff : faVolumeUp} fixedWidth/>
-                                    </span>
-                                </a>
-                                <div style={{width: '120px'}}>
-                                    <SliderWithTooltip trackStyle={{ backgroundColor: 'hsl(141, 71%, 48%)', height: 4 }}
-                                                       railStyle={{backgroundColor: '#ddd'}}
-                                                       handleStyle={{borderColor: 'hsl(141, 71%, 48%)'}}
-                                                       value={volume} min={-30} max={0} step={1}
-                                                       tipFormatter={v => `${v}dB`}
-                                                       onChange={this.handleVolumeChange} />
-                                </div>
-                            </div>
-                        </div>
+                        {levelLeft}
+                        {levelRight}
                     </nav>
                 </div>
             </div>

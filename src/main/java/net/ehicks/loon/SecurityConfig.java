e4d6230e08c44230aecae137c6866061439a01db
Eric Hicks <ehicks05@yahoo.com>
2019-07-03 15:59:34 -0400

    Avoid possible race condition where spring responds to a browser plugin (like wappalyzer) requesting /robots.txt when user is trying to log on.

--- src/main/java/net/ehicks/loon/SecurityConfig.java
+++ src/main/java/net/ehicks/loon/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/login", "/register", "/images/**", "/js/dist/**", "/styles/**").permitAll()
+                .antMatchers("/login", "/register", "/images/**", "/js/dist/**", "/styles/**", "/robots.txt").permitAll()
                 .antMatchers("/api/admin/**").hasRole("ADMIN")
                 .antMatchers("/**").hasRole("USER")
                 .and()

07e55537e20c1731913539de4910ea5304512e5a
Eric Hicks <ehicks05@yahoo.com>
2019-11-08 12:13:16 -0500

    Add client logic to perform the playlist reordering for drag-and-drop. This duplicates what the backend does, but lets us render the reordered playlist without waiting for a request/response cycle. Previously, after doing a drag-and-drop, the list would revert to it's previous order for ~1/2 second before rendering the new ordering. Very unsatisfying to use.

--- src/main/resources/static/js/stores/AppState.js
+++ src/main/resources/static/js/stores/AppState.js
@@ -1,8 +1,9 @@
-import {observable, computed, autorun, action} from 'mobx';
+import {observable, computed, action} from 'mobx';
+import {createTransformer} from "mobx-utils";
 
 export class AppState {
     @observable tracks;
-    @observable playlists;
+    @observable playlists = [];
     @observable systemSettings = [];
     @observable versionInfo = [];
     @observable pendingRequests = 0;
@@ -17,6 +18,9 @@ export class AppState {
         this.loadPlaylists();
         this.loadVersionInfo();
     }
+
+    getPlaylistById = createTransformer(id => this.playlists.find(playlist => playlist.id === id));
+    getTrackById = createTransformer(id => this.tracks.find(track => track.id === id));
 
     @action
     logout()
@@ -37,7 +41,7 @@ export class AppState {
                         if (o1.index > o2.index) return 1;
                     })
                 });
-                this.playlists = data;
+                this.playlists.replace(data);
             });
     }
 
@@ -89,9 +93,38 @@ export class AppState {
             .then(data => {this.loadPlaylists();});
     }
 
+    // This will will update the playlist indices locally so the change can be rendered immediately,
+    // then request the backend to do the same logic and return the updated playlist data back to the client.
+    // This data should be identical to the updates we made locally, but in case anything goes wrong
+    // the backend will be our source of truth.
     @action
     dragAndDrop(formData) {
-        return this.rootStore.myFetch('/api/playlists/dragAndDrop', {method: 'POST', body: formData})
+        const oldIndex = Number(formData.get('oldIndex'));
+        const newIndex = Number(formData.get('newIndex'));
+        const low = Math.min(oldIndex, newIndex);
+        const high = Math.max(oldIndex, newIndex);
+        const adjustBy = newIndex < oldIndex ? 1 : -1;
+        const playlistId = Number(formData.get('playlistId'));
+
+        const playlistIndex = this.playlists.findIndex(playlist => playlist.id === playlistId);
+        const playlist = this.playlists[playlistIndex];
+
+        let updatedTracks = playlist.playlistTracks.slice();
+        updatedTracks.forEach(playlistTrack => {
+            if (playlistTrack.index >= low && playlistTrack.index <= high) {
+                playlistTrack.index = playlistTrack.index === oldIndex ? newIndex : playlistTrack.index + adjustBy
+            }
+        });
+
+        updatedTracks = updatedTracks.sort((o1, o2) => {
+            if (o1.index === o2.index) return 0;
+            if (o1.index < o2.index) return -1;
+            if (o1.index > o2.index) return 1;
+        });
+
+        playlist.playlistTracks = updatedTracks;
+
+        this.rootStore.myFetch('/api/playlists/dragAndDrop', {method: 'POST', body: formData})
             .then(response => response.text())
             .then(data => {this.loadPlaylists();});
     }

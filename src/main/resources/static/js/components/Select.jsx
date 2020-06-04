cbf6ec6d996ec4fcc236ab5fa26700a31e6c100e
Eric Hicks <ehicks05@yahoo.com>
2020-06-03 20:44:14 -0400

    Convert Select.jsx and TextInput.jsx to functional components.

--- src/main/resources/static/js/components/Select.jsx
+++ src/main/resources/static/js/components/Select.jsx
@@ -1,60 +1,53 @@
 import React from 'react';
 
-export default class Select extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+export default function Select(props) {
 
-    render()
-    {
-        const label = this.props.label;
-        const isStatic = this.props.isStatic ? 'is-static' : '';
-        const isHorizontal = this.props.horizontal;
-        const id = this.props.id;
-        const value = this.props.value;
-        const blankLabel = this.props.blankLabel;
-        const required = this.props.required;
-        const items = this.props.items;
-        const multiple = this.props.multiple;
-        const size = this.props.size ? this.props.size : null;
+    const label = props.label;
+    const isHorizontal = props.horizontal;
+    const id = props.id;
+    const value = props.value;
+    const blankLabel = props.blankLabel;
+    const required = props.required;
+    const items = props.items;
+    const multiple = props.multiple;
+    const size = props.size ? props.size : null;
 
-        if (!required)
-            items.splice(0, 0, {value: '', text: blankLabel});
+    if (!required)
+        items.splice(0, 0, {value: '', text: blankLabel});
 
-        const labelEl = isHorizontal ?
-            <div className="field-label is-normal">
-                <label className="label">{label}</label>
+    const labelEl = isHorizontal ?
+        <div className="field-label is-normal">
+            <label className="label">{label}</label>
+        </div>
+        :
+        <label className="label">{label}</label>;
+
+    const options = items.map((item, index) =>
+        <option key={index} value={item.value}>{item.text}</option>
+    );
+
+    let select =
+        <div className="control">
+            <div className={"select " + (multiple ? 'is-multiple' : '')}>
+                <select id={id} name={id} defaultValue={value} multiple={multiple} size={size} style={{overflow: 'auto'}}>
+                    {options}
+                </select>
             </div>
-            :
-            <label className="label">{label}</label>;
+        </div>;
 
-        const options = items.map((item, index) =>
-            <option key={index} value={item.value}>{item.text}</option>
-        );
-
-        let select =
-            <div className="control">
-                <div className={"select " + (multiple ? 'is-multiple' : '')}>
-                    <select id={id} name={id} defaultValue={value} multiple={multiple} size={size} style={{overflow: 'auto'}}>
-                        {options}
-                    </select>
-                </div>
-            </div>;
-
-        select = isHorizontal ?
-            <div className="field-body">
-                <div className="field is-narrow">
-                    {select}
-                </div>
-            </div>
-            :
-            select
-        ;
-
-        return (
-            <div className={"field " + (isHorizontal ? 'is-horizontal' : '')}>
-                {labelEl}
+    select = isHorizontal ?
+        <div className="field-body">
+            <div className="field is-narrow">
                 {select}
-            </div>);
-    }
+            </div>
+        </div>
+        :
+        select
+    ;
+
+    return (
+        <div className={"field " + (isHorizontal ? 'is-horizontal' : '')}>
+            {labelEl}
+            {select}
+        </div>);
 }
--- src/main/resources/static/js/components/TextInput.jsx
+++ src/main/resources/static/js/components/TextInput.jsx
@@ -1,85 +1,76 @@
 import React from 'react';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {inject, observer} from "mobx-react";
 
-@inject('store')
-@observer
-export default class TextInput extends React.Component {
-    constructor(props) {
-        super(props);
-    }
+export default function TextInput(props) {
 
-    render()
-    {
-        const label = this.props.label;
-        const hideLabel = this.props.hideLabel;
-        const id = this.props.id;
-        const value = this.props.value;
-        const required = this.props.required ? 'required' : '';
-        const size = this.props.size ? this.props.size : 20;
-        const autoComplete = this.props.autoComplete ? this.props.autoComplete : '';
-        let isHorizontal = this.props.horizontal;
-        const autofocus = this.props.autofocus;
+    const label = props.label;
+    const hideLabel = props.hideLabel;
+    const id = props.id;
+    const value = props.value;
+    const required = props.required ? 'required' : '';
+    const size = props.size ? props.size : 20;
+    const autoComplete = props.autoComplete ? props.autoComplete : '';
+    let isHorizontal = props.horizontal;
+    const autofocus = props.autofocus;
 
-        const leftIcon = this.props.leftIcon;
+    const leftIcon = props.leftIcon;
 
-        const onChange = this.props.onChange;
+    const onChange = props.onChange;
 
-        let fieldClass = 'field';
-        fieldClass += isHorizontal ? ' is-horizontal' : '';
-        let labelClass = 'field-label';
-        labelClass += isHorizontal ? ' is-normal' : '';
+    let fieldClass = 'field';
+    fieldClass += isHorizontal ? ' is-horizontal' : '';
+    let labelClass = 'field-label';
+    labelClass += isHorizontal ? ' is-normal' : '';
 
-        let controlClass = 'control';
-        controlClass += leftIcon ? ' has-icons-left' : '';
+    let controlClass = 'control';
+    controlClass += leftIcon ? ' has-icons-left' : '';
 
-        let inputClass = 'input ';
-        inputClass += this.props.isStatic ? ' is-static ' : '';
-        inputClass += this.props.inputClass ? this.props.inputClass : '';
+    let inputClass = 'input ';
+    inputClass += props.isStatic ? ' is-static ' : '';
+    inputClass += props.inputClass ? props.inputClass : '';
 
-        const labelEl = !hideLabel ? (isHorizontal ? (
-            <div className={labelClass}>
-                <label className="label">{label}</label>
-            </div>
-        ) : (
+    const labelEl = !hideLabel ? (isHorizontal ? (
+        <div className={labelClass}>
             <label className="label">{label}</label>
-        )) : '';
+        </div>
+    ) : (
+        <label className="label">{label}</label>
+    )) : '';
 
-        const leftIconEl = leftIcon ? (
-            <span className="icon is-small is-left">
-                <FontAwesomeIcon icon={leftIcon} aria-hidden="true" />
-            </span>
-        ) : '';
+    const leftIconEl = leftIcon ? (
+        <span className="icon is-small is-left">
+            <FontAwesomeIcon icon={leftIcon} aria-hidden="true" />
+        </span>
+    ) : '';
 
-        const controlEl = (
-            <div className={controlClass}>
-                <input className={inputClass}
-                       type="text"
-                       size={size}
-                       placeholder={label}
-                       id={id}
-                       name={id}
-                       defaultValue={value}
-                       required={required}
-                       onChange={onChange}
-                       autoFocus={autofocus}
-                       autoComplete={autoComplete}
-                />
-                {leftIconEl}
-            </div>);
+    const controlEl = (
+        <div className={controlClass}>
+            <input className={inputClass}
+                   type="text"
+                   size={size}
+                   placeholder={label}
+                   id={id}
+                   name={id}
+                   defaultValue={value}
+                   required={required}
+                   onChange={onChange}
+                   autoFocus={autofocus}
+                   autoComplete={autoComplete}
+            />
+            {leftIconEl}
+        </div>);
 
-        const fieldEl = isHorizontal ? (
-            <div className="field-body">
-                <div className="field">
-                    {controlEl}
-                </div>
+    const fieldEl = isHorizontal ? (
+        <div className="field-body">
+            <div className="field">
+                {controlEl}
             </div>
-        ) : (controlEl);
+        </div>
+    ) : (controlEl);
 
-        return (
-            <div className={fieldClass}>
-                {labelEl}
-                {fieldEl}
-            </div>);
-    }
+    return (
+        <div className={fieldClass}>
+            {labelEl}
+            {fieldEl}
+        </div>);
 }

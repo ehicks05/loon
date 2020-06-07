8adef0ce8b73de00fd533b9c5fc344383b4fcc7b
Eric Hicks <ehicks05@yahoo.com>
2020-06-07 01:03:00 -0400

    Remove table from GeneralSettings.js.

--- src/main/resources/static/js/components/GeneralSettings.js
+++ src/main/resources/static/js/components/GeneralSettings.js
@@ -28,18 +28,10 @@ export default function GeneralSettings() {
             </section>
             <section className="section">
                 <form id="frmGeneralSettings" method="post" action="">
-                    <table className={'table'} style={{padding: '8px', maxWidth: '80%', borderCollapse: 'separate'}}>
-                        <tbody>
-                        <tr>
-                            <td style={{padding: '10px'}} className={'has-text-centered'}>
-                                <div className="field">
-                                    <input type="checkbox" className="switch is-rounded" id="transcode" name="transcode" checked={userContext.user.userState.transcode} onChange={(e) => setTranscode(e)} />
-                                    <label htmlFor="transcode">Transcode all tracks to mp3 v{transcodeQuality}</label>
-                                </div>
-                            </td>
-                        </tr>
-                        </tbody>
-                    </table>
+                    <div className="field">
+                        <input type="checkbox" className="switch is-rounded" id="transcode" name="transcode" checked={userContext.user.userState.transcode} onChange={(e) => setTranscode(e)} />
+                        <label htmlFor="transcode">Transcode all tracks to mp3 v{transcodeQuality}</label>
+                    </div>
                 </form>
             </section>
         </div>

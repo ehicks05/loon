e44171bb4c877528f14282303cc45d5e0dfb0302
Eric Hicks <ehicks05@yahoo.com>
2020-06-12 12:29:28 -0400

    Add a side panel indicator icon to show the currently selected playlist. This is not the playlist that is currently being rendered, but the one that is providing the pool of tracks to play from.

--- src/main/resources/static/js/components/global/SidePanel.js
+++ src/main/resources/static/js/components/global/SidePanel.js
@@ -1,16 +1,22 @@
 import React, {useContext} from 'react';
 import {NavLink} from "react-router-dom";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faSearch, faMusic, faUsers, faFolderOpen, faHeart, faList, faCompactDisc} from '@fortawesome/free-solid-svg-icons'
+import {faSearch, faMusic, faUsers, faFolderOpen, faHeart, faList, faCompactDisc, faRedo} from '@fortawesome/free-solid-svg-icons'
 import {AppContext} from "../../common/AppContextProvider";
+import {UserContext} from "../../common/UserContextProvider";
 
 export default function SidePanel() {
     const appContext = useContext(AppContext);
+    const userContext = useContext(UserContext);
+    const selectedPlaylistId = userContext.user.userState.selectedPlaylistId;
+
+    if (!appContext || !appContext.playlists || !userContext)
+        return <div>Loading...</div>;
 
     const defaultLinks = [
-        {path: '/search', icon: faSearch, text: 'Search'},
-        {path: '/favorites', icon: faHeart, text: 'Favorites'},
-        {path: '/queue', icon: faList, text: 'Queue'},
+        {path: '/search', icon: faSearch, text: 'Search', currentlyPlaying: selectedPlaylistId === 0},
+        {path: '/favorites', icon: faHeart, text: 'Favorites', currentlyPlaying: selectedPlaylistId === appContext.playlists.find(playlist => playlist.favorites).id},
+        {path: '/queue', icon: faList, text: 'Queue', currentlyPlaying: selectedPlaylistId === appContext.playlists.find(playlist => playlist.queue).id},
         {path: '/artists', icon: faUsers, text: 'Artists'},
         {path: '/albums', icon: faCompactDisc, text: 'Albums'},
         {path: '/playlists', icon: faFolderOpen, text: 'Playlists'},
@@ -18,25 +24,29 @@ export default function SidePanel() {
 
     const links = defaultLinks.map(link => linkToNavLink(link));
 
-    if (!appContext || !appContext.playlists)
-        return <div>Loading...</div>;
-
     let playlists = appContext.playlists
         .filter(playlist => !playlist.favorites && !playlist.queue)
         .map((playlist) => playlistToLink(playlist))
         .map((link) => linkToNavLink(link));
 
     function playlistToLink(playlist) {
-        return {path: '/playlists/' + playlist.id, icon: faMusic, text: playlist.name}
+        return {path: '/playlists/' + playlist.id, icon: faMusic, text: playlist.name, currentlyPlaying: playlist.id === selectedPlaylistId}
     }
 
     function linkToNavLink(link) {
+        const currentlyPlayingIcon = link.currentlyPlaying ?
+            <span className="panel-icon has-text-success" style={{marginLeft: '.75em'}}>
+                <FontAwesomeIcon icon={faRedo} spin aria-hidden="true"/>
+            </span>
+            : null;
+
         return (
             <NavLink key={link.path} to={link.path} className={'panel-block'} activeClassName={'is-active'}>
-                    <span className="panel-icon">
-                        <FontAwesomeIcon icon={link.icon} aria-hidden="true"/>
-                    </span>
+                <span className="panel-icon">
+                    <FontAwesomeIcon icon={link.icon} aria-hidden="true"/>
+                </span>
                 {link.text}
+                {currentlyPlayingIcon}
             </NavLink>
         )
     }

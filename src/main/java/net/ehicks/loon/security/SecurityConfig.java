046b2321d0b92c0d92e711c6c5d7ddb0e9a9fbd5
Eric Hicks <ehicks05@yahoo.com>
2021-03-28 17:49:31 -0400

    cors config

--- src/main/java/net/ehicks/loon/security/SecurityConfig.java
+++ src/main/java/net/ehicks/loon/security/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
         @Override
         protected void configure(HttpSecurity http) throws Exception {
             http
-                    .cors(withDefaults())
+                    .cors().and()
                     .authorizeRequests()
                     .antMatchers("/login", "/me", "/register", "/images/**", "/art/**", "/js/dist/**", "/styles/**", "/robots.txt").permitAll()
                     .antMatchers("/api/admin/**").hasRole("ADMIN")
@@ -69,21 +69,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
                     .and().csrf().disable()
             ;
         }
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        final CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8082/", "https://loon.ehicks.net", "http://localhost:3000"));
-        configuration.setAllowedMethods(List.of("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH"));
-        // setAllowCredentials(true) is important, otherwise:
-        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' when the request's credentials mode is 'include'.
-        configuration.setAllowCredentials(true);
-        // setAllowedHeaders is important! Without it, OPTIONS preflight request
-        // will fail with 403 Invalid CORS request
-        configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 }

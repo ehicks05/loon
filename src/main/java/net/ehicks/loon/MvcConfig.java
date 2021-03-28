b84cefe6755beafef52ec3a7dc430fa7c0bb64f5
Eric Hicks <ehicks05@yahoo.com>
2021-03-28 17:37:47 -0400

    start cors config

--- src/main/java/net/ehicks/loon/MvcConfig.java
+++ src/main/java/net/ehicks/loon/MvcConfig.java
@@ -1,6 +1,7 @@
 package net.ehicks.loon;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -22,5 +23,10 @@ public class MvcConfig implements WebMvcConfigurer
                 .setCachePeriod(3600)
                 .resourceChain(true)
                 .addResolver(new PathResourceResolver());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("https://loon.ehicks.net");
     }
 }

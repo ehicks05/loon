b3a5b7ef4bd13a283a9d4fd5dafad97d930db61a
Eric Hicks <ehicks05@yahoo.com>
2021-03-28 17:57:27 -0400

    view controller

--- src/main/java/net/ehicks/loon/MvcConfig.java
+++ src/main/java/net/ehicks/loon/MvcConfig.java
@@ -3,18 +3,12 @@ package net.ehicks.loon;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer
 {
-    public void addViewControllers(ViewControllerRegistry registry)
-    {
-        registry.addViewController("/login").setViewName("login");
-    }
-
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry

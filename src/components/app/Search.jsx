1e50874262bb066e0860251579fc36ed5ed631a4
Eric Hicks <ehicks05@yahoo.com>
2024-07-18 11:52:18 -0400

    extract TrackListing

--- src/components/app/Search.jsx
+++ src/components/app/Search.jsx
@@ -1,33 +1,68 @@
+import _ from "lodash";
 import React, { useEffect, useRef, useState } from "react";
-import { FixedSizeList as List } from 'react-window';
-import MediaItem from "../MediaItem";
-import TextInput from "../TextInput";
 import { FaSearch } from "react-icons/fa";
+import { useMeasure } from "react-use";
+import { FixedSizeList as List } from "react-window";
 import { useAppStore } from "../../common/AppContextProvider";
 import {
-  useUserStore,
   setSelectedContextMenuId,
+  useUserStore,
 } from "../../common/UserContextProvider";
-import _ from "lodash";
+import MediaItem from "../MediaItem";
+import TextInput from "../TextInput";
 
-const Row = ({ data, index, style }) => {
-  const track = data[index];
-
-  return <div style={style}>
-    <MediaItem playlistId={0} track={track} trackNumber={index + 1} />
+const Row = ({ data, index, style }) => (
+  <div style={style}>
+    <MediaItem playlistId={0} track={data[index]} trackNumber={index + 1} />
   </div>
-}
+);
+
+const TrackListing = ({ tracks }) => {
+  const listRef = useRef();
+  const [containerRef, { height: containerHeight }] = useMeasure();
+
+  const selectedTrackId = useUserStore(
+    (state) => state.userState.selectedTrackId,
+  );
+
+  useEffect(() => {
+    return function cleanup() {
+      setSelectedContextMenuId(null);
+    };
+  }, []);
+
+  const getIndex = tracks.indexOf(
+    tracks.find((track) => track.id === selectedTrackId),
+  );
+
+  const scrollToIndex = () => {
+    const index = tracks.find((track) => track.id === selectedTrackId);
+    listRef.current.scrollToItem(index, "center");
+  };
+
+  return (
+    <div ref={containerRef} className="flex h-full flex-grow flex-col">
+      <List
+        ref={listRef}
+        width="100%"
+        height={containerHeight}
+        itemCount={tracks.length}
+        itemData={tracks}
+        itemKey={(_index, data) => data.id}
+        itemSize={60}
+      >
+        {Row}
+      </List>
+    </div>
+  );
+};
 
 export default function Search() {
-  const listRef = useRef();
-  const [searchResults, setSearchResults] = useState([]);
   const [searchKey, setSearchKey] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
   const setSearchKeyDebounced = _.debounce(setSearchKey, 200);
 
   const tracks = useAppStore((state) => state.tracks);
-  const selectedTrackId = useUserStore(
-    (state) => state.userState.selectedTrackId
-  );
 
   useEffect(() => {
     return function cleanup() {
@@ -56,31 +91,13 @@ export default function Search() {
     setSearchKeyDebounced(e.target.value);
   }
 
-  const getIndex = searchResults.indexOf(
-    searchResults.find((track) => track.id === selectedTrackId)
-  );
-
-  const scrollToIndex = () => listRef.current.scrollToItem(getIndex(), "center");
-
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        height: "100%",
-        flex: "1",
-        overflow: "hidden",
-      }}
-    >
-      <section
-        className={"section"}
-        style={{ display: "flex", flexDirection: "column" }}
-      >
+    <div className="flex h-full flex-col overflow-hidden">
+      <section className="section flex flex-col">
         <form>
           <TextInput
-            id={"searchInput"}
-            label={"Search"}
-            leftIcon={<FaSearch color={"gray"} />}
+            label="Search"
+            leftIcon={<FaSearch color="gray" />}
             value={searchKey}
             onChange={handleSearchInput}
             horizontal={false}
@@ -90,27 +107,7 @@ export default function Search() {
         </form>
       </section>
 
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          height: "100%",
-          flex: "1",
-          flexGrow: 1,
-        }}
-      >
-        <List
-          ref={listRef}
-          width={'100%'}
-          height={800}
-          itemCount={searchResults.length}
-          itemData={searchResults}
-          itemKey={(_index, data) => data.id}
-          itemSize={60}
-        >
-          {Row}
-        </List>
-      </div>
+      <TrackListing tracks={searchResults} />
     </div>
   );
 }

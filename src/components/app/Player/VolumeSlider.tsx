4fcf3530132686a1031d9a4968289c37ead18dc6
Eric Hicks <ehicks05@yahoo.com>
2024-07-19 14:45:31 -0400

    remove tooltip

--- src/components/app/Player/VolumeSlider.tsx
+++ src/components/app/Player/VolumeSlider.tsx
@@ -3,30 +3,20 @@ import React from "react";
 import { setVolume, useUserStore } from "../../../common/UserContextProvider";
 import "rc-slider/assets/index.css";
 
-const createSliderWithTooltip = Slider.createSliderWithTooltip;
-
-const SliderWithTooltip = createSliderWithTooltip(Slider);
-
-const volumeSliderStyle = { width: "120px", marginRight: ".75rem" };
-const sliderTrackStyle = { backgroundColor: "hsl(141, 71%, 48%)", height: 4 };
-const sliderRailStyle = { backgroundColor: "#ddd" };
-const sliderHandleStyle = { borderColor: "hsl(141, 71%, 48%)" };
-
 export default function VolumeSlider() {
   const volume = useUserStore((state) => state.userState.volume);
 
   return (
-    <div style={volumeSliderStyle}>
-      <SliderWithTooltip
-        trackStyle={sliderTrackStyle}
-        railStyle={sliderRailStyle}
-        handleStyle={sliderHandleStyle}
+    <div className="w-32 mr-3">
+      <Slider
         value={volume}
         min={-30}
         max={0}
         step={1}
-        tipFormatter={(v) => `${v}dB`}
         onChange={setVolume}
+        trackStyle={{ backgroundColor: "hsl(141, 71%, 48%)", height: 4 }}
+        railStyle={{ backgroundColor: "#ddd" }}
+        handleStyle={{ borderColor: "hsl(141, 71%, 48%)" }}
       />
     </div>
   );

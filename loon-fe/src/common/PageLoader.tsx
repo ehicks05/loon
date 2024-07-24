c8505fdcae7dd8048f45de715b42880bf99d7174
Eric Hicks <ehicks05@yahoo.com>
2024-07-24 10:29:15 -0400

    loader style

--- loon-fe/src/common/PageLoader.tsx
+++ loon-fe/src/common/PageLoader.tsx
@@ -3,14 +3,8 @@ import { RevolvingDot } from "react-loader-spinner";
 
 export default function PageLoader() {
   return (
-    <div
-      style={{
-        width: "100vw",
-        height: "100vh",
-        display: "flex",
-      }}
-    >
-      <div style={{ margin: "auto" }}>
+    <div className="h-dvh w-dvw flex bg-neutral-950">
+      <div className="m-auto">
         <RevolvingDot color="#44CC44" height={150} width={150} />
       </div>
     </div>

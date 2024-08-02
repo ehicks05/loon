eda291501c0c893c1c055770a1e77bbfbb30b67b
Eric Hicks <ehicks05@yahoo.com>
2024-08-02 11:16:41 -0400

    volume hover label

--- loon-fe/src/components/BottomPanel/VolumeSlider.tsx
+++ loon-fe/src/components/BottomPanel/VolumeSlider.tsx
@@ -1,11 +1,20 @@
 import { setVolume, useUserStore } from "../../common/UserContextProvider";
 import { LoonSlider } from "../Slider";
 
+const VolumeLabel = ({ volume }: { volume: number }) => (
+  <div className="absolute right-0 bottom-6 opacity-0 group-hover:opacity-100 transition-all">
+    <span className="text-right text-green-600 text-sm font-semibold">
+      {volume}dB
+    </span>
+  </div>
+);
+
 export default function VolumeSlider() {
   const volume = useUserStore((state) => state.userState.volume);
 
   return (
-    <div className="w-32 max-w-32 md:w-auto md:flex-grow mr-1">
+    <div className="w-32 max-w-32 md:w-auto md:flex-grow mr-1 relative group">
+      <VolumeLabel volume={volume} />
       <LoonSlider
         value={[volume]}
         onValueChange={(value) => setVolume(value[0])}

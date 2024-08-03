ca1c4577268d134e7e948c0f82a3c5038256b58a
Eric Hicks <ehicks05@yahoo.com>
2024-08-03 13:49:16 -0400

    refactor

--- loon-fe/src/components/SidePanel.tsx
+++ loon-fe/src/components/SidePanel.tsx
@@ -1,7 +1,6 @@
 import { useAppStore } from "@/common/AppContextProvider";
 import { useUserStore } from "@/common/UserContextProvider";
 import type { Playlist } from "@/common/types";
-import type { ReactNode } from "react";
 import {
   FaCompactDisc,
   FaFolderOpen,
@@ -12,12 +11,13 @@ import {
   FaUsers,
   FaVolumeUp,
 } from "react-icons/fa";
+import type { IconType } from "react-icons/lib";
 import { NavLink } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
 interface PlaylistLink {
   path: string;
-  icon: ReactNode;
+  icon: IconType;
   text: string;
   currentlyPlaying?: boolean;
 }
@@ -25,13 +25,7 @@ interface PlaylistLink {
 function playlistToLink(playlist: Playlist, selectedPlaylistId: string) {
   return {
     path: `/playlists/${playlist.id}`,
-    icon: playlist.favorites ? (
-      <FaHeart />
-    ) : playlist.queue ? (
-      <FaList />
-    ) : (
-      <FaMusic />
-    ),
+    icon: playlist.favorites ? FaHeart : playlist.queue ? FaList : FaMusic,
     text: playlist.name,
     currentlyPlaying: playlist.id === selectedPlaylistId,
   };
@@ -42,23 +36,24 @@ interface SidebarLinkProps {
   isPlaylist?: boolean;
 }
 
-const SidebarLink = ({ link, isPlaylist }: SidebarLinkProps) => {
+const SidebarLink = ({
+  link: { path, text, icon: Icon, currentlyPlaying },
+  isPlaylist,
+}: SidebarLinkProps) => {
   const location = useLocation();
-  const isActive = location.pathname === link.path;
+  const isActive = location.pathname === path;
+  const linkClasses = `flex items-center justify-between p-2 ${isPlaylist ? "pl-6" : "pl-4"} hover:bg-neutral-800 transition-all rounded-lg`;
 
   return (
-    <NavLink
-      key={link.path}
-      to={link.path}
-      exact
-      className={`flex gap-2 items-center p-2 ${isPlaylist ? "pl-6" : "pl-4"} rounded-lg hover:bg-neutral-800 transition-all ${isActive ? "text-white" : ""}`}
-    >
-      <span className="w-4 h-4">{link.icon}</span>
-      {link.text}
-      {link.currentlyPlaying && (
-        <span className="w-4 h-4 text-green-500 ml-3" title={"Active Playlist"}>
-          <FaVolumeUp aria-hidden="true" />
-        </span>
+    <NavLink key={path} to={path} exact className={linkClasses}>
+      <div
+        className={`flex gap-2 items-center ${isActive ? "text-white" : ""}`}
+      >
+        <Icon />
+        {text}
+      </div>
+      {currentlyPlaying && (
+        <FaVolumeUp className="text-green-500" title="Now Playing" />
       )}
     </NavLink>
   );
@@ -70,26 +65,19 @@ export default function SidePanel() {
     (state) => state.userState.selectedPlaylistId,
   );
 
-  if (!playlists) return <div>Loading...</div>;
-
   const defaultLinks = [
     {
       path: "/search",
-      icon: <FaSearch />,
+      icon: FaSearch,
       text: "Search",
       currentlyPlaying: selectedPlaylistId === "",
     },
-    { path: "/artists", icon: <FaUsers />, text: "Artists" },
-    { path: "/albums", icon: <FaCompactDisc />, text: "Albums" },
-    { path: "/library", icon: <FaFolderOpen />, text: "Library" },
+    { path: "/artists", icon: FaUsers, text: "Artists" },
+    { path: "/albums", icon: FaCompactDisc, text: "Albums" },
+    { path: "/library", icon: FaFolderOpen, text: "Library" },
   ];
 
-  const favorites = playlists.find((p) => p.favorites);
-  const queue = playlists.find((p) => p.queue);
-  const rest = playlists
-    .filter((p) => !p.favorites && !p.queue)
-    .sort((o1, o2) => o1.id.localeCompare(o2.id));
-  const playlistLinks = [favorites, queue, ...rest]
+  const playlistLinks = playlists
     .filter((o): o is Playlist => !!o)
     .map((playlist) => playlistToLink(playlist, selectedPlaylistId));
 

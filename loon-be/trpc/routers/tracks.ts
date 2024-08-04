efa957ed4e2d4e532bd6a91f8e57cede910b41bc
Eric Hicks <ehicks05@yahoo.com>
2024-08-04 19:22:42 -0400

    sort tracks

--- loon-be/trpc/routers/tracks.ts
+++ loon-be/trpc/routers/tracks.ts
@@ -1,10 +1,14 @@
+import { asc } from "drizzle-orm";
 import { db } from "../../db";
 import { tracks } from "../../drizzle/main";
 import { publicProcedure, router } from "../trpc";
 
 export const tracksRouter = router({
   list: publicProcedure.query(async () => {
-    const result = await db.select().from(tracks);
+    const result = await db
+      .select()
+      .from(tracks)
+      .orderBy(asc(tracks.artist), asc(tracks.album), asc(tracks.title));
     return result;
   }),
 });

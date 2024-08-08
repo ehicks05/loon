c82a2a0664839d46cf7097324bef040f2af8bd37
Eric Hicks <ehicks05@yahoo.com>
2024-08-07 22:07:32 -0400

    simplify

--- loon-fe/src/app/Album.tsx
+++ loon-fe/src/app/Album.tsx
@@ -5,7 +5,6 @@ import { useAppStore } from "@/common/AppContextProvider";
 import AlbumCard from "@/components/AlbumCard";
 import { sortBy } from "lodash";
 import { useRouteMatch } from "react-router-dom";
-import { useWindowSize } from "react-use";
 
 export default function Album() {
   const {
@@ -13,8 +12,6 @@ export default function Album() {
   } = useRouteMatch<{ artist: string; album: string }>();
 
   const tracks = useAppStore((state) => state.tracks);
-  const maxWidth = useWindowSize().width >= 768 ? "100%" : "500px";
-
   const albumTracks = sortBy(
     tracks.filter(
       (track) => track.albumArtist === artist && track.album === album,
@@ -22,45 +19,25 @@ export default function Album() {
     ["discNumber", "trackNumber"],
   );
 
-  const mediaItems = albumTracks.map((track) => {
-    return (
-      <MediaItem
-        key={track.id}
-        playlistId={""}
-        track={track}
-        trackNumber={`${track.discNumber}.${track.trackNumber}`}
-      />
-    );
-  });
-
   return (
-    <section className={"section"}>
-      <div className="columns">
-        <div className="column is-one-third">
-          <div style={{ maxWidth: maxWidth, margin: "auto" }}>
-            <AlbumCard
-              album={{
-                artist: albumTracks[0].albumArtist,
-                name: albumTracks[0].album,
-                imageId: albumTracks[0].spotifyAlbumImageThumb,
-              }}
-            />
-          </div>
-        </div>
-        <div className="column">
-          <ul
-            id="list"
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              flex: "1",
-              flexGrow: "1",
-            }}
-          >
-            {mediaItems}
-          </ul>
-        </div>
-      </div>
+    <section>
+      <AlbumCard
+        album={{
+          artist: albumTracks[0]?.albumArtist,
+          name: albumTracks[0]?.album,
+          imageId: albumTracks[0]?.spotifyAlbumImageThumb,
+        }}
+      />
+      <ul className="flex flex-col">
+        {albumTracks.map((track) => (
+          <MediaItem
+            key={track.id}
+            playlistId={""}
+            track={track}
+            trackNumber={`${track.discNumber}.${track.trackNumber}`}
+          />
+        ))}
+      </ul>
     </section>
   );
 }

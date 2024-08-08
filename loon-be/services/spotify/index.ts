f80298b4b6124e8568be5a189cb97540956d5740
Eric Hicks <ehicks05@yahoo.com>
2024-08-08 14:58:51 -0400

    try catch

--- loon-be/services/spotify/index.ts
+++ loon-be/services/spotify/index.ts
@@ -3,14 +3,14 @@ import { spotify } from "./client";
 
 export type LoonItemTypes = Extract<ItemTypes, "artist" | "album">;
 
-const imageCache: Record<LoonItemTypes, Record<string, Image[]>> = {
+export const imageCache: Record<LoonItemTypes, Record<string, Image[]>> = {
   artist: {},
   album: {},
 };
 
-const toFullAndThumb = (images: Image[]) => ({
-  full: images[0] || "",
-  thumb: images.length > 1 ? images[1] : images[0] || "",
+export const toFullAndThumb = (images: Image[]) => ({
+  full: images[0],
+  thumb: images.length > 1 ? images[1] : images[0],
 });
 
 export const fetchImages = async ({
@@ -22,9 +22,14 @@ export const fetchImages = async ({
     return toFullAndThumb(cachedResult);
   }
 
-  const result = await spotify.search(q, [itemType]);
+  let result = undefined;
+  try {
+    result = await spotify.search(q, [itemType]);
+  } catch (e) {
+    console.error(e);
+  }
   const resultEntity =
-    itemType === "artist" ? result.artists.items[0] : result.albums.items[0];
+    itemType === "artist" ? result?.artists.items[0] : result?.albums.items[0];
   if (!resultEntity) {
     console.log(`unable to find ${itemType} ${q}`);
   }

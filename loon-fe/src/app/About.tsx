4be0e06e01e8884b7b9a5d70a5e03be7f89832c2
Eric Hicks <ehicks05@yahoo.com>
2024-08-19 14:24:15 -0400

    json

--- loon-fe/src/app/About.tsx
+++ loon-fe/src/app/About.tsx
@@ -3,29 +3,14 @@ import { useUserStore } from "@/hooks/useUserStore";
 
 export default function About() {
   const selectedTrackId = useUserStore((state) => state.selectedTrackId);
-  const selectedTrack = getTrackById(selectedTrackId);
+  const track = getTrackById(selectedTrackId);
 
   return (
     <section className="flex flex-col gap-4">
       <div className="text-2xl font-bold">Selected Track Info</div>
-      <table className="text-left">
-        <thead>
-          <tr>
-            <th>Field</th>
-            <th>Value</th>
-          </tr>
-        </thead>
-        <tbody>
-          {selectedTrack
-            ? Object.entries(selectedTrack).map(([key, val]) => (
-                <tr key={key}>
-                  <td>{key}</td>
-                  <td>{val?.toString()}</td>
-                </tr>
-              ))
-            : null}
-        </tbody>
-      </table>
+      <div className="whitespace-pre-wrap font-mono text-sm">
+        {JSON.stringify(track, null, 2)}
+      </div>
     </section>
   );
 }

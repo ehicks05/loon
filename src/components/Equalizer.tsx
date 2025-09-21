b27cc05abaeeffaebb39126a50822d944c7bf8c3
Eric Hicks <ehicks05@yahoo.com>
2025-09-21 13:08:12 -0400

    wip

--- src/components/Equalizer.tsx
+++ src/components/Equalizer.tsx
@@ -1,5 +1,5 @@
 import { LoonSlider } from '@/components/Slider';
-import { type EqBand, useUser } from '@/hooks/useUser';
+import { type EqBand as IEqBand, useUser } from '@/hooks/useUser';
 
 const FILTER_TYPE_LABELS: Partial<Record<BiquadFilterType, string>> = {
 	lowshelf: 'Low Shelf',
@@ -7,67 +7,58 @@ const FILTER_TYPE_LABELS: Partial<Record<BiquadFilterType, string>> = {
 	highshelf: 'High Shelf',
 };
 
-export const Equalizer = () => {
+const EqBand = ({ eq }: { eq: IEqBand }) => {
 	const { eqBands, setEqBands } = useUser();
 
-	const handleUpdate = (newBand: EqBand, id: number) =>
+	const handleUpdate = (newBand: IEqBand, id: number) =>
 		setEqBands(eqBands.map((band) => (band.id === id ? newBand : band)));
 
 	return (
-		<table>
-			<tbody>
-				<tr>
-					{eqBands.map((eq) => (
-						<td key={eq.id} className={'text-center text-xs p-1'}>
-							{FILTER_TYPE_LABELS[eq.type]}
-						</td>
-					))}
-				</tr>
-				<tr>
-					{eqBands.map((eq) => (
-						<td key={eq.id} className="p-1">
-							<div className="flex flex-col items-center h-56">
-								<LoonSlider
-									value={[eq.gain]}
-									onValueChange={(value) =>
-										handleUpdate({ ...eq, gain: value[0] }, eq.id)
-									}
-									onDoubleClick={() => handleUpdate({ ...eq, gain: 0 }, eq.id)}
-									min={-12}
-									max={12}
-									step={1}
-									orientation="vertical"
-								/>
-								<span>
-									{eq.gain > 0 ? '+' : ''}
-									{eq.gain} dB
-								</span>
-							</div>
-						</td>
-					))}
-				</tr>
-				<tr>
-					{eqBands.map((eq) => (
-						<td key={eq.id} className="p-1">
-							<div className="flex gap-1 items-baseline pr-2 bg-neutral-800">
-								<input
-									className="bg-neutral-800 w-12 text-right [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
-									type="number"
-									min={20}
-									max={20000}
-									step={1}
-									value={eq.frequency}
-									onChange={(e) => {
-										const frequency = Number(e.target.value);
-										handleUpdate({ ...eq, frequency }, eq.id);
-									}}
-								/>
-								<span className="text-xs">hz</span>
-							</div>
-						</td>
-					))}
-				</tr>
-			</tbody>
-		</table>
+		<div className="flex flex-col gap-2">
+			<div className="text-center text-xs p-1">{FILTER_TYPE_LABELS[eq.type]}</div>
+			<div className="flex flex-col items-center h-56">
+				<LoonSlider
+					value={[eq.gain]}
+					onValueChange={(value) => handleUpdate({ ...eq, gain: value[0] }, eq.id)}
+					onDoubleClick={() => handleUpdate({ ...eq, gain: 0 }, eq.id)}
+					min={-12}
+					max={12}
+					step={1}
+					orientation="vertical"
+				/>
+				<span>
+					{eq.gain > 0 ? '+' : ''}
+					{eq.gain} dB
+				</span>
+			</div>
+
+			<div className="flex gap-1 items-baseline pr-2 bg-neutral-800">
+				<input
+					className="bg-neutral-800 w-12 text-right [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+					type="number"
+					min={20}
+					max={20000}
+					step={1}
+					value={eq.frequency}
+					onChange={(e) => {
+						const frequency = Number(e.target.value);
+						handleUpdate({ ...eq, frequency }, eq.id);
+					}}
+				/>
+				<span className="text-xs">hz</span>
+			</div>
+		</div>
+	);
+};
+
+export const Equalizer = () => {
+	const { eqBands } = useUser();
+
+	return (
+		<div className="flex gap-2">
+			{eqBands.map((eq) => (
+				<EqBand key={eq.id} eq={eq} />
+			))}
+		</div>
 	);
 };

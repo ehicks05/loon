55756b83701fe02593a8286f1b8162298051d5e3
Eric Hicks <ehicks05@yahoo.com>
2025-10-17 12:56:04 -0400

    copy from simple example

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,17 @@
-import path from "path";
 import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import tsConfigPaths from 'vite-tsconfig-paths';
+import viteTsConfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from "@tailwindcss/vite";
 import { nitro } from "nitro/vite";
-import { nitroV2Plugin } from '@tanstack/nitro-v2-vite-plugin';
 
 export default defineConfig({
-	server: {
-		port: 3000,
-		host: '0.0.0.0',
-	},
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
 	plugins: [
-		tsConfigPaths(),
-		tanstackStart({ 
-			public: { base: '/images', dir: 'public' }
-		}),
-		// nitroV2Plugin({
-		// 	preset: 'node-server',
-		// 	compatibilityDate: '2025-09-25',
-		// }),
-		nitro({
-			config: {
-				preset: 'node-server',
-				compatibilityDate: '2025-09-25'
-			},
-		}),
+		tanstackStart(),
+		nitro(),
+    viteTsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
 		tailwindcss(),
 		viteReact(),
 	],
